5f3d3379938ff0c40aca31037496eaead418e0fd
mason park <joung4342@gmail.com>
2024-05-20 21:53:52 +0900

    feat(multi-module): 프로젝트 구성

---
+++ modules/endpoint/module-core-api/src/main/java/com/business/endpoint/ModuleCoreApiWebApplication.java
@@ -0,0 +1,11 @@
+package com.business.endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleCoreApiWebApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(ModuleCoreApiWebApplication.class, args);
+    }
+}
