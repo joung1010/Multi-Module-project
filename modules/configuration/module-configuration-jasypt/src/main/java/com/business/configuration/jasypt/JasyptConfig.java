78732adaaaab34bf2a3c4c89632d0cfb8f6f348c
mason park <joung4342@gmail.com>
2024-05-27 20:52:56 +0900

    refactor(multi-module): JasyptConfig 설정 모듈 추가

---
+++ modules/configuration/module-configuration-jasypt/src/main/java/com/business/configuration/jasypt/JasyptConfig.java
@@ -0,0 +1,40 @@
+package com.business.configuration.jasypt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <b>  </b>
+ *
+ * @author jh.park
+ * @version 0.1.0
+ * @since 2024-05-27
+ */
+
+@Slf4j
+@Configuration
+public class JasyptConfig {
+
+    public static final String BEAN_NAME = "jasyptStringEncryptor";
+
+    @Bean(name = BEAN_NAME)
+    public StringEncryptor stringEncryptor(@Value("${jasypt.encryptor.password}") String password) {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        EnvironmentStringPBEConfig config = new EnvironmentStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndTripleDES");
+        config.setPoolSize(1);
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
---
+++ modules/configuration/module-meta-configuration/src/main/java/com/business/configuration/component/ConfigurationScan.java
@@ -0,0 +1,17 @@
+package com.business.configuration.component;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <b>  </b>
+ *
+ * @author jh.park
+ * @version 0.1.0
+ * @since 2024-05-27
+ */
+
+@ComponentScan(basePackages = "com.business.configuration")
+@Configuration
+public class ConfigurationScan {
+}
