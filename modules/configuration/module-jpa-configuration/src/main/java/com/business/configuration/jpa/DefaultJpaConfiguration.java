2d24e8ad85b902b18d7aeee4b1e0d1842fc357d7
mason park <joung4342@gmail.com>
2024-07-17 21:26:23 +0900

    fix(business): jpa 설정 수정

--- modules/configuration/module-jpa-configuration/src/main/java/com/business/configuration/jpa/DefaultJpaConfiguration.java
+++ modules/configuration/module-jpa-configuration/src/main/java/com/business/configuration/jpa/DefaultJpaConfiguration.java
@@ -59,7 +59,7 @@ public class DefaultJpaConfiguration {
     private Properties hibernateProperties() {
         Properties properties = new Properties();
         properties.put("hibernate.hbm2ddl.auto", "update");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.format_sql", "true");
         return properties;
