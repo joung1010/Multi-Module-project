40880bc4f5877fc6b169ef4b40a0c01f0b29f88a
mason park <joung4342@gmail.com>
2024-07-17 21:24:59 +0900

    feat(business): 테스트 스타터 모듈 추가

---
+++ modules/configuration/module-test-starter/src/main/java/com/business/ModuleTestApplicationStarter.java
@@ -0,0 +1,18 @@
+package com.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b>  </b>
+ *
+ * @author jh.park
+ * @version 0.1.0
+ * @since 2024-07-17
+ */
+@SpringBootApplication
+public class ModuleTestApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleTestApplicationStarter.class, args);
+    }
+}
