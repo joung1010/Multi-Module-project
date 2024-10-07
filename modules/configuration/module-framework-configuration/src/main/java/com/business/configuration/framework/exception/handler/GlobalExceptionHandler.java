b051ab8050976ba73e6b04343f6d163d24b645b5
mason park <joung4342@gmail.com>
2024-10-07 20:10:27 +0900

    feat(business): Exception 처리 로직추가

--- modules/configuration/module-framework-configuration/src/main/java/com/business/configuration/framework/exception/handler/GlobalExceptionHandler.java
+++ modules/configuration/module-framework-configuration/src/main/java/com/business/configuration/framework/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.business.configuration.framework.exception.handler;
 
-import com.business.configuration.framework.exception.BusinessException;
+import com.business.configuration.framework.exception.*;
 import com.business.configuration.framework.standard.http.CoreHttpResponseEntity;
 import com.business.configuration.framework.standard.http.body.CoreHttpResponseBodyEntity;
 import com.business.configuration.framework.standard.http.provider.CoreHttpResponseProvider;
@@ -26,4 +26,38 @@ public class GlobalExceptionHandler {
         return CoreHttpResponseProvider.responseFail(ex.getErrorCode());
     }
 
+    @ExceptionHandler(BadRequestException.class)
+    public CoreHttpResponseEntity<CoreHttpResponseBodyEntity<Object>> handleBadRequestException(BadRequestException ex) {
+        log.error("BadRequestException occurred: {}", ex.getMessage());
+
+        return CoreHttpResponseProvider.responseFail(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public CoreHttpResponseEntity<CoreHttpResponseBodyEntity<Object>> handleDatabaseException(DatabaseException ex) {
+        log.error("DatabaseException occurred: {}", ex.getMessage());
+
+        return CoreHttpResponseProvider.responseFail(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public CoreHttpResponseEntity<CoreHttpResponseBodyEntity<Object>> handleAuthorizationException(AuthorizationException ex) {
+        log.error("AuthorizationException occurred: {}", ex.getMessage());
+
+        return CoreHttpResponseProvider.responseFail(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public CoreHttpResponseEntity<CoreHttpResponseBodyEntity<Object>> handleAuthenticationException(AuthenticationException ex) {
+        log.error("AuthenticationException occurred: {}", ex.getMessage());
+
+        return CoreHttpResponseProvider.responseFail(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(ApplicationException.class)
+    public CoreHttpResponseEntity<CoreHttpResponseBodyEntity<Object>> handleApplicationException(ApplicationException ex) {
+        log.error("ApplicationException occurred: {}", ex.getMessage());
+
+        return CoreHttpResponseProvider.responseFail(ex.getErrorCode());
+    }
 }
