80fec294a42c5309e0b90437752e1c15b4da1153
mason park <joung4342@gmail.com>
2024-10-21 21:21:00 +0900

    feat(business): 엔티티추가

---
+++ modules/business/module-core-business/src/main/java/com/business/domain/TMemberRole.java
@@ -0,0 +1,44 @@
+package com.business.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "T_MEMBER_ROLE", schema = "mbr_v10")
+public class TMemberRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "role", nullable = false, length = 50)
+    private String role;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "member_id", nullable = false)
+    private TMemberEntity member;
+
+}
