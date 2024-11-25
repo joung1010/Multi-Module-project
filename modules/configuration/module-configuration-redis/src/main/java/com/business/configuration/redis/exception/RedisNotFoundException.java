378d1f623b19bbd6daf2cb13d17fee84809fc615
mason park <joung4342@gmail.com>
2024-11-25 19:53:27 +0900

    feat(business): resilience4j 설정추가

---
+++ modules/configuration/module-configuration-redis/src/main/java/com/business/configuration/redis/exception/RedisNotFoundException.java
@@ -0,0 +1,13 @@
+package com.business.configuration.redis.exception;
+
+import com.business.configuration.framework.exception.DatabaseException;
+
+/**
+ * <b> RedisNotFoundException </b>
+ *
+ * @author jh.park
+ * @version 0.1.0
+ * @since 2024-11-25
+ */
+public class RedisNotFoundException extends DatabaseException {
+}
