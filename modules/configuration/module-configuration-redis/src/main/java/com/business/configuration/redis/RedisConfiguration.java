9a0fd9ba52a21e26a2b90fdd7cba3d1f9a049926
mason park <joung4342@gmail.com>
2024-12-02 20:43:21 +0900

    fix(business): 레디스 설정 오류 수정

--- modules/configuration/module-configuration-redis/src/main/java/com/business/configuration/redis/RedisConfiguration.java
+++ modules/configuration/module-configuration-redis/src/main/java/com/business/configuration/redis/RedisConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -29,6 +30,7 @@ import java.time.Duration;
  */
 @Configuration
 @RequiredArgsConstructor
+@EnableRedisRepositories(basePackages = "com.business.**.repository")
 @EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfiguration {
     private final RedisProperties properties;
